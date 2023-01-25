901806ae47ed0dc9940e34663c7b1dbafd537f4f
HiimHyy <quochuytran130804@gmail.com>
2023-01-25 15:43:49 +0200

    Add project files.

---
+++ Task 5 generic collection/Program.cs
@@ -0,0 +1,21 @@
+﻿//Create an instance
+
+using System.Reflection;
+using Task_5_generic_collection;
+Answer result;
+
+List<Student> students = new List<Student>()
+{
+    new Student("Joel",new List<int> { 5, 7, 9, 10, 4 }),
+    new Student("Jane",new List<int> { 8, 7, 9, 5, 7 }),
+    new Student("Heidi",new List<int> { 9, 10, 7, 3, 5 }),
+    new Student("Elisabeth",new List<int> { 8, 6, 6, 8, 10 })
+};
+foreach (var item in students)
+{
+    Console.WriteLine("Student {0} got sum total of {1} points", item.Name, item.CalculateSum());
+    Console.WriteLine("{0}'s grade is {1}.", item.Name, item.CalculateGrade());
+    result = item.FindExamWithMinPoints();
+    Console.WriteLine("Minimum points were {0} points.", result.Points);
+    Console.WriteLine("It was the {0}. exam.", result.WhichOne);
+}
---
+++ Task 5 generic collection/Student.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5_generic_collection
+{
+    struct Answer
+    {
+        public Answer(int points, int whichOne) 
+        {
+            Points = points;
+            WhichOne = whichOne;
+        }
+        public int Points { get; set; }
+        public int WhichOne { get; set; }
+    }
+    internal class Student
+    {
+        public Student(string name, List<int> examPoints) 
+        {
+            Name = name;
+            ExamPoints = examPoints;
+        }
+
+        public string Name { get; set; }
+        public List<int> ExamPoints { get; set; }
+
+        //Instance methods here:
+
+        public int CalculateSum()
+        {
+            int numberOfElements = ExamPoints.Count;
+            int sumTotal = 0;
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                sumTotal = sumTotal + ExamPoints[i];
+            }
+            return sumTotal;
+        }
+        public int CalculateGrade()
+        {
+            int grade;
+            int sumTotal = CalculateSum();
+            if (sumTotal < 26)
+                grade = 0;
+            else if (sumTotal < 31)
+                grade = 1;
+            else if (sumTotal < 36)
+                grade = 2;
+            else if (sumTotal < 41)
+                grade = 3;
+            else if (sumTotal < 46)
+                grade = 4;
+            else
+                grade = 5;
+            return grade;
+        }
+        public Answer FindExamWithMinPoints()
+        {
+            int smallest = ExamPoints.Min();
+            int index = ExamPoints.IndexOf(ExamPoints.Min());
+            Answer answer = new Answer(smallest, index);
+            return answer;
+        }
+    }
+}
